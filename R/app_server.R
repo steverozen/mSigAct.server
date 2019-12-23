2d2ab45897fe7e34512bb9742689315e698f7310
jnh01 <44010028+jnh01@users.noreply.github.com>
2019-12-23 18:23:37 +0800

    working draft version

--- R/app_server.R
+++ R/app_server.R
@@ -2,41 +2,77 @@
 #' @import shiny
 app_server <- function(input, output,session) {
   # List the first level callModules here
+  
+  volumes <- getVolumes()
+  shinyDirChoose(input, "directory", roots = volumes, session = session)
+  output$dir <- renderText(parseDirPath(volumes, input$directory))
+  
+  observeEvent(input$submit, {
+    output$download.zipfile <- 
+      downloadHandler(filename = paste0(input$zipfile.name, ".zip"),
+                      content = function(file) {
+                        
+                        dir <- parseDirPath(volumes, input$directory)
+                        trans.ranges <- reactive({
+                          if (input$ref.genome == "hg19") {
+                            return(trans.ranges.GRCh37)
+                          } else if (input$ref.genome == "hg38") {
+                            return(trans.ranges.GRCh38)
+                          } else if (input$ref.genome == "mm10") {
+                            return(trans.ranges.GRCm38)
+                          }
+                        })
+                        
+                        names.of.VCFs <- reactive({
+                          if (input$names.of.VCFs == "") {
+                            return(NULL)
+                          } else {
+                            vector <- unlist(strsplit(input$names.of.VCFs, 
+                                                      ",", fixed = TRUE))
+                            return(trimws(vector))
+                          }
+                        })
+                        
+                        if (input$vcf.type == "strelka.sbs") {
+                          StrelkaSBSVCFFilesToZipFile(dir,
+                                                      file,
+                                                      input$ref.genome, 
+                                                      trans.ranges(),
+                                                      input$region, 
+                                                      names.of.VCFs(),
+                                                      input$output.file,
+                                                      input$zipfile.name)
+                        } else if (input$vcf.type == "strelka.id") {
+                          StrelkaIDVCFFilesToZipFile(dir,
+                                                     file,
+                                                     input$ref.genome,
+                                                     input$region,
+                                                     names.of.VCFs(),
+                                                     input$output.file,
+                                                     input$zipfile.name)
+                        } else if (input$vcf.type == "mutect") {
+                          tumor.col.names <- reactive({
+                            if (input$tumor.col.names == "NA") {
+                              return (NA)
+                            } else {
+                              vector1 <- unlist(strsplit(input$tumor.col.names, 
+                                                        ",", fixed = TRUE))
+                              return(trimws(vector1))
+                            }
+                          })
+                          MutectVCFFilesToZipFile(dir,
+                                                  file,
+                                                  input$ref.genome,
+                                                  trans.ranges(),
+                                                  input$region,
+                                                  names.of.VCFs(),
+                                                  tumor.col.names(),
+                                                  input$output.file,
+                                                  input$zipfile.name)
+                        }
+                      })
+    output$download.status <- eventReactive(input$submit, "Ready to download")
+  }
+      )
 
-  files <- renderText({input$file$datapath})
-  vcf.type <- isolate(input$vcf.type)
-  ref.genome <- isolate(input$ref.genome)
-  if (ref.genome == "hg19") {
-    trans.ranges <- trans.ranges.GRCh37
-  } else if (ref.genome == "hg38") {
-    trans.ranges <- trans.ranges.GRCh38
-  } else if (ref.genome == "mm10") {
-    trans.ranges <- trans.ranges.GRCm38
-  }
-  region <- isolate(input$region)
-  
-  names.of.VCFs <- isolate(input$names.of.VCFs)
-  if (names.of.VCFs == "") {
-    names.of.VCFs <- NULL
-  }
-  
-  output.file <- isolate(input$output.file)
-  zipfile.name <- isolate(input$zipfile.name)
-  
-  #browser()
- 
-  cats <-
-    observeEvent(input$file, 
-                 StrelkaSBSVCFFilesToZipFile(input$file$datapath, 
-                                             ref.genome, 
-                                             trans.ranges,
-                                             region, names.of.VCFs,
-                                             output.file,
-                                             zipfile.name))
-  
-  
-  browser()
-  output$value <- files
-  
-  
 }
