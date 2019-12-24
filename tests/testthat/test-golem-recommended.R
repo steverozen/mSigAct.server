e4b1045613ebb54b5dbb1c31f60339ffe1e12295
jnh01 <44010028+jnh01@users.noreply.github.com>
2019-12-24 10:38:37 +0800

    Update test-golem-recommended.R

--- tests/testthat/test-golem-recommended.R
+++ tests/testthat/test-golem-recommended.R
@@ -1,6 +1,7 @@
 context("golem tests")
 
 library(golem)
+library(ICAMS)
 
 test_that("app ui", {
   ui <- app_ui()
@@ -12,24 +13,6 @@ test_that("app server", {
   expect_is(server, "function")
 })
 
-# Configure this test to fit your need
-test_that(
-  "app launches",{
-    skip_on_cran()
-    skip_on_travis()
-    skip_on_appveyor()
-    x <- processx::process$new(
-      "R", 
-      c(
-        "-e", 
-        "setwd('../../'); pkgload::load_all();run_app()"
-      )
-    )
-    Sys.sleep(5)
-    expect_true(x$is_alive())
-    x$kill()
-  }
-)
 
 
 
