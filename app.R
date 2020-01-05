be404c41f4001b23868d2b5dceff956c1fab7b2b
jnh01 <44010028+jnh01@users.noreply.github.com>
2020-01-05 22:22:32 +0800

    Update app.R

--- app.R
+++ app.R
@@ -1,6 +1,10 @@
 # Launch the ShinyApp (Do not remove this comment)
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
+# options("repos" = BiocManager::repositories())
+# imports BSgenome.Hsapiens.1000genomes.hs37d5,
+# imports BSgenome.Hsapiens.UCSC.hg38,
+# imports BSgenome.Mmusculus.UCSC.mm10
 
 pkgload::load_all()
 options( "golem.app.prod" = TRUE)
