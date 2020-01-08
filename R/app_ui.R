910532feb9c4d43357d310ccf81bf686453fef05
jnh01 <44010028+jnh01@users.noreply.github.com>
2020-01-08 17:30:06 +0800

    Update app_ui.R

--- R/app_ui.R
+++ R/app_ui.R
@@ -1,5 +1,4 @@
 #' @import shiny
-#' @import shinyFiles
 #' @import shinythemes
 app_ui <- function() {
   tagList(
@@ -7,101 +6,86 @@ app_ui <- function() {
     golem_add_external_resources(),
     # List the first level UI elements here 
     fluidPage(
+      # Specify the theme used for UI
       theme = shinytheme("cerulean"),
-      titlePanel(p(a("ICAMS", href = "https://github.com/steverozen/ICAMS"),
-                   ": In-depth Characterization and Analysis of ", 
-                   "Mutational Signatures")),
       
+      # Add a title on top the page
+      titlePanel(p("ICAMS: In-depth Characterization and Analysis of ",
+                 "Mutational Signatures")),
+      
+      # Add a horizontal line
       hr(),
+      
+      # Add the first row of control widgets
       fluidRow(
-        column(3, add_type_of_vcf()),
-        column(3, add_reference_genome()),
-        column(3, add_region()),
-        column(3, conditionalPanel(
+        # Add radio buttons for user to specify the type of VCF files
+        column(4, add_vcf_type()),
+        
+        # Add a conditional panel for user to specify the column names in 
+        # Mutect VCFs which contain the tumor sample information (if needed)
+        column(4, conditionalPanel(
           condition = "input.vcftype == 'mutect'",
           add_tumor_col_names()))
       ),
-      add_common_parameters(),
-      h5(strong("Please select a folder which contains the VCF files")),
+      
+      # Add the next row of control widgets
       fluidRow(
-        column(1,
-               shinyDirButton(id = "directory", label = "Folder select", 
-                              title = "Please select a folder which contains the VCF files"),
-               textOutput("dir")),
-        column(3,
-               actionButton(inputId = "submit", label = "Update argument and submit"))
+        # Add radio buttons for user to specify the reference genome
+        column(4, add_reference_genome()),
+        
+        # Add radio buttons for user to specify the genomic region
+        # from where the VCFs were generated
+        column(4, add_region())
       ),
+      
+      # Add the next row of control widgets
+      fluidRow(
+        # Add text input for user to specify the sample names
+        # representing different VCF files
+        column(4, add_sample_names()),
+        
+        # Add text input for user to specify the base filename
+        # of the CSV and PDF files generated
+        column(4, add_base_filename()),
+        
+        # Add text input for user to specify the zip file name
+        column(4, add_zipfile_name())
+      ),
+      
+      # Add a file upload control for user to upload multiple VCF files
+      fileInput(inputId = "vcf.files", label = "Choose VCF Files", 
+                multiple = TRUE),
+      
+      # Add an action button for user to update argument and submit
+      actionButton(inputId = "submit", label = "Update argument and submit"),
+      
+      # Add two line breaks
       rep_br(2),
+      
+      # Add a download button appearing as soon as when user clicks the 
+      # action button to update argument and submit for the first time
       conditionalPanel(
-        condition = "output.showbutton == 'yes'",
-        downloadButton(outputId = "download.zipfile", label = "Download Zip file")
-      ),
+        condition = "output.clicksubmit",
+        downloadButton(outputId = "download", label = "Download")),
+      
+      verbatimTextOutput(outputId = "id"),
+      
+      # Add two line breaks
       rep_br(2),
+      
+      # Add a horizontal line
       hr(),
+      
+      # Add a footer on the page showing the URL of ICAMS package
       p("For complete documentation of ICAMS, please refer to ",
         a(href = "https://cran.rstudio.com/web/packages/ICAMS/index.html",
           "https://cran.rstudio.com/web/packages/ICAMS/index.html"))
-     
+      
     )
   )
 }
 
-#' @import shiny
-add_type_of_vcf <- function() {
-  radioButtons(inputId = "vcftype", label = h5(strong("Type of VCF")), 
-               choiceNames = list(p(a(href = "https://github.com/Illumina/strelka", "Strelka"),
-                                    " single base substitutions (SBS)"),
-                                  p(a(href = "https://github.com/Illumina/strelka", "Strelka"),
-                                    " Indel (ID)"),
-                                  a(href = "https://github.com/broadgsa/gatk", "Mutect")),
-               choiceValues = list("strelka.sbs", "strelka.id", "mutect"),
-               selected = "strelka.sbs")
-}
 
-#' @import shiny
-add_reference_genome <- function(){
-  radioButtons(inputId = "ref.genome", label = h5(strong("Reference genome")), 
-               choiceNames = list(p("Human GRCh37/hg19"), p("Human GRCh38/hg38"),
-                                  p("Mouse GRCm38/mm10")),
-               choiceValues = list("hg19", "hg38", "mm10"),
-               selected = "hg19")
-}
-
-#' @import shiny
-add_region <- function(){
-  radioButtons(inputId = "region", label = h5(strong("Genomic region")), 
-               choices = c("unknown", "genome", "exome", "transcript"), 
-               selected = "unknown")
-}
-
-#' @import shiny
-add_tumor_col_names <- function() {
-  textInput(inputId = "tumor.col.names", 
-            label = p(strong(h5("Names of columns in Mutect VCFs containing", 
-                              "tumor sample information")),
-                      h5("(e.g. colname1, colname2, colname3 ... ", 
-                         "If value is NA(default), the program will use the ", 
-                         "10th column in all the VCFs to calculate VAFs)")),
-            value = "NA")
-}
-
-#' @import shiny
-add_common_parameters <- function(){
-  fluidRow(
-    column(3, textInput(inputId = "names.of.VCFs", 
-                        label = p(strong(h5("Sample names")),
-                                  h5("(e.g. name1, name2, name3 ...If ", 
-                                     "blank, then use the names of the VCF files.)")))),
-    column(3, textInput(inputId = "output.file",
-                        label = p(strong(h5("Base name of the CSV and PDF files to create")),
-                                  h5("(e.g. name1, or you may leave it blank)")))),
-    column(3, textInput(inputId = "zipfile.name",
-                        label = p(strong(h5("Name of zip file to create")),
-                                  h5("(e.g. name1, or you may use the default value", 
-                                     " ICAMS.output)")),
-                        value = "ICAMS.output"))
-  )
-}
 
 #' @import shiny
 golem_add_external_resources <- function(){
