06ea284e9a5a05e41a25cacb6db0fa35df96e95a
jnh01 <44010028+jnh01@users.noreply.github.com>
2020-04-16 00:58:11 +0800

    Update app_server.R

--- R/app_server.R
+++ R/app_server.R
@@ -2,63 +2,54 @@
 app_server <- function(input, output,session) {
   # List the first level callModules here
   
-  # Create output values that can be used as condition argument
-  # in the function conditionalPanel() in app_ui.R
-  output$clicksubmit <- reactive({input$submit})
-  output$usebuiltindata <- reactive({input$builtindata})
-  outputOptions(output, "clicksubmit", suspendWhenHidden = FALSE) 
-  outputOptions(output, "usebuiltindata", suspendWhenHidden = FALSE) 
-  
   # Create an empty list which can be used to store notification ids later
   ids <- list("error" = character(0), "warning" = character(0), 
              "message" = character(0))
   
-  # Download sample VCFs when user clicks the buttom
-  output$downloadsampleVCFs <- downloadHandler(filename = "sample-VCFs.zip", 
-                                             content = function(file) {
-                                               PrepareSampleVCFs(file)
-                                             })
+  # Download sample VCFs when user clicks the button
+  output$downloadsampleVCFs <- 
+    downloadHandler(filename = "sample-VCFs.zip", 
+                    content = function(file) {
+                      PrepareSampleVCFs(file)
+                    })
   
-  # Generate the zip archive if user chooses to use built-in data
-  observeEvent(
-    input$builtindata,
-    {
-      output$download <- 
-        downloadHandler(filename = "ICAMS-test.zip",
-                        content = function(file) {
-                          #browser()
-                          ids <<- 
-                            GenerateZipFileFromBuiltInData(output, file, ids)
-                        })
-    })
+  # Run ICAMS on sample Strelka SBS VCFs when user clicks the button
+  output$runstrelkasbsvcfs <- 
+    downloadHandler(filename = "ICAMS-test-run-Strelka-SBS-VCFs.zip",
+                    content = function(file) {
+                      ids <<- 
+                        RunICAMSOnSampleStrelkaSBSVCFs(output, file, ids)
+                    })
   
-  # Only when user clicks the submit button, then all the reactive values
-  # used as parameters in generating the zip archive will be updated
-  observeEvent(
-    input$submit,
-    {
-      output$download <- 
-        downloadHandler(filename = paste0(input$zipfile.name, ".zip"),
-                        content = function(file) {
-                          if (input$vcftype == "strelka.sbs") {
-                            # Generate a zip archive from Strelka SBS VCFs and
-                            # update the notification ids for errors, warnings
-                            # and messages
-                            ids <<- 
-                              ProcessStrelkaSBSVCFs(input, output, file, ids)
-                          } else if (input$vcftype == "strelka.id") {
-                            ids <<- ProcessStrelkaIDVCFs(input, output, file, ids)
-                          } else if (input$vcftype == "mutect") {
-                            ids <<- ProcessMutectVCFs(input, output, file, ids)
-                          }
-                        })
+  # Run ICAMS on sample Mutect VCFs when user clicks the button
+  output$runmutectvcfs <- 
+    downloadHandler(filename = "ICAMS-test-run-Mutect-VCFs.zip",
+                    content = function(file) {
+                      ids <<- 
+                        RunICAMSOnSampleMutectVCFs(output, file, ids)
+                    })
+  
+  # When user clicks the submit button, then the program will try to 
+  # generate a zip archive based on the input files and parameters
+  output$download <- 
+    downloadHandler(filename = paste0(input$zipfile.name, ".zip"),
+                    content = function(file) {
+                      if (input$vcftype == "strelka.sbs") {
+                        # Generate a zip archive from Strelka SBS VCFs and
+                        # update the notification ids for errors, warnings
+                        # and messages
+                        ids <<- 
+                          ProcessStrelkaSBSVCFs(input, output, file, ids)
+                      } else if (input$vcftype == "strelka.id") {
+                        ids <<- ProcessStrelkaIDVCFs(input, output, file, ids)
+                      } else if (input$vcftype == "mutect") {
+                        ids <<- ProcessMutectVCFs(input, output, file, ids)
+                      }
+                    })
         
-    }
-  )
-  
-  # When user clicks the submit buttom, all the previous notifications(error,
-  # warning or message) will be removed
-  observeEvent(input$submit, {
+  # When user clicks the "Remove notifications" button, all the previous
+  # notifications(error, warning or message) will be removed
+  observeEvent(input$remove, {
     RemoveAllNotifications(ids)
   })
 }
