14c01345e0e6972e74efe0a4e1e36c0cdf49b072
jnh01 <44010028+jnh01@users.noreply.github.com>
2020-11-06 05:39:54 +0800

    Added tabs on the top of the page

--- R/app_ui.R
+++ R/app_ui.R
@@ -3,147 +3,154 @@ app_ui <- function() {
   tagList(
     # Leave this function for adding external resources
     golem_add_external_resources(),
-    # List the first level UI elements here 
-    fixedPage(
-      # Add a title on top the page
-      titlePanel(title = p("ICAMS: In-depth Characterization and Analysis of ",
-                           "Mutational Signatures", style = "color: #337ab7"),
-                 windowTitle = paste0("ICAMS: In-depth Characterization and ", 
-                                 "Analysis of Mutational Signatures")),
-      
-      # Add a horizontal line
-      hr(),
-      
-      # Add a short description of the shiny interface of ICAMS
-      p("The ", a(href = "https://shiny.rstudio.com/", "Shiny"), 
-        "interface of ",
-        a(href = "https://cran.r-project.org/web/packages/ICAMS/index.html", "ICAMS"), 
-        " allows users to upload multiple ", 
-        a(href = "https://tinyurl.com/rdzwnxd", "VCF"), 
-        " (Variant Call Format) files and returns a zip archive which ",
-        "contains mutation catalogs and PDF plots. ",
-        "Only VCF files from ",
-        a(href = "https://github.com/Illumina/strelka", "Strelka"), " or ",
-        a(href = "https://github.com/broadgsa/gatk", "Mutect"), 
-        " variant caller are supported.",
-        "The uploaded VCFs must ", strong("all"), " come from the ", 
-        strong("same"), " variant caller, reference genome and region."),
-         
-      # Add a link to the COSMIC website about mutational signatures
-      p("For background information of mutational signatures, please refer ", 
-        "to this paper: ",
-        a(href = "https://doi.org/10.1038/s41586-020-1943-3", 
-          "The repertoire of mutational signatures in human cancer"), "."),
-      
-      # Add an overview picture about the Shiny interface
-      img(src = "www/ICAMS.shiny-overview-v2.JPG", width = "800", height = "471"),
     
-      # Add a horizontal line
-      hr(),
-      
-      # Add the first row of control widgets
-      fixedRow(
-        # Add radio buttons for user to specify the type of VCF files
-        column(6, add_vcf_type()),
-        
-        # Add a conditional panel for user to specify the column names in 
-        # Mutect VCFs which contain the tumor sample information (if needed)
-        column(6, conditionalPanel(
-          condition = "input.vcftype == 'mutect'",
-          add_tumor_col_names()))
-      ),
-      
-      # Add the next row of control widgets
-      fixedRow(
-        # Add radio buttons for user to specify the reference genome
-        column(6, add_reference_genome()),
-        
-        # Add radio buttons for user to specify the genomic region
-        # from where the VCFs were generated
-        column(6, add_region())
-      ),
-      
-      # Add the next row of control widgets
-      fixedRow(
-        # Add text input for user to specify the sample names
-        # representing different VCF files
-        column(6, add_sample_names()),
-        
-        # Add text input for user to specify the base filename
-        # of the CSV and PDF files generated
-        column(6, add_base_filename())
-      ),
-      
-      # Add the next row of control widgets
-      fixedRow(
-        # Add text input for user to specify the zip file name
-        column(6, add_zipfile_name()),
-        
-        # Add a file upload control for user to upload multiple VCF files
-        column(6, upload_vcf_files())),
-      
-      
-      # Add the next row of control widgets
-      fixedRow(column(6,
-                      MyDownloadButton(outputId = "download",
-                                       label = "Submit",
-                                       style="color: #fff; 
-                                       background-color: #337ab7; 
-                                       border-color: #2e6da4"),
-                      offset = 6)),
-      
-      # Add one line break
-      br(),
-      
-      fixedRow(column(6,
-                      actionButton(inputId = "remove", 
-                                   label = "Remove notifications"),
-                      offset = 6)),
-      
-      # Add one line break
-      br(),
-      
-      # Add a download button for user to download VCF files to test
-      fixedRow(column(6,
-                      downloadButton(outputId = "downloadsampleVCFs", 
-                                     label = "Download the sample VCFs"),
-                      offset = 6)),
-      
-      # Add one line break
-      br(),
-      
-      # Add a button for user to run ICAMS on sample Strelka SBS VCFs
-      fixedRow(column(6, 
-                      MyDownloadButton(outputId = "runstrelkasbsvcfs", 
-                                       label = 
-                                         paste0("Run ICAMS on two ", 
-                                                "1-sample Strelka SBS VCFs")),
-                      offset = 6)),
-      
-      # Add one line break
-      br(),
-      
-      # Add a button for user to run ICAMS on sample Mutect VCFs
-      fixedRow(column(6, 
-                      MyDownloadButton(outputId = "runmutectvcfs", 
-                                       label = 
-                                         paste0("Run ICAMS on two ", 
-                                                "1-sample Mutect VCFs")),
-                      offset = 6)),
-      
-      # Add a horizontal line
-      hr(),
-      
-      # Add a footer on the page showing the URL of ICAMS package
-      p("For complete documentation of ICAMS, please refer to ",
-        a(href = "https://cran.rstudio.com/web/packages/ICAMS/index.html",
-          "https://cran.rstudio.com/web/packages/ICAMS/index.html"))
-      
+    tabsetPanel(
+      tabPanel("ICAMS.shiny", MyTab()),
+      tabPanel("Analysis", "test")
     )
   )
 }
 
-
+#' @import shiny
+MyTab <- function() {
+  # List the first level UI elements here 
+  fixedPage(
+    # Add a title on top the page
+    titlePanel(title = p("ICAMS: In-depth Characterization and Analysis of ",
+                         "Mutational Signatures", style = "color: #337ab7"),
+               windowTitle = paste0("ICAMS: In-depth Characterization and ", 
+                                    "Analysis of Mutational Signatures")),
+    
+    # Add a horizontal line
+    hr(),
+    
+    # Add a short description of the shiny interface of ICAMS
+    p("The ", a(href = "https://shiny.rstudio.com/", "Shiny"), 
+      "interface of ",
+      a(href = "https://cran.r-project.org/web/packages/ICAMS/index.html", "ICAMS"), 
+      " allows users to upload multiple ", 
+      a(href = "https://tinyurl.com/rdzwnxd", "VCF"), 
+      " (Variant Call Format) files and returns a zip archive which ",
+      "contains mutation catalogs and PDF plots. ",
+      "Only VCF files from ",
+      a(href = "https://github.com/Illumina/strelka", "Strelka"), " or ",
+      a(href = "https://github.com/broadgsa/gatk", "Mutect"), 
+      " variant caller are supported.",
+      "The uploaded VCFs must ", strong("all"), " come from the ", 
+      strong("same"), " variant caller, reference genome and region."),
+    
+    # Add a link to the COSMIC website about mutational signatures
+    p("For background information of mutational signatures, please refer ", 
+      "to this paper: ",
+      a(href = "https://doi.org/10.1038/s41586-020-1943-3", 
+        "The repertoire of mutational signatures in human cancer"), "."),
+    
+    # Add an overview picture about the Shiny interface
+    img(src = "www/ICAMS.shiny-overview-v2.JPG", width = "800", height = "471"),
+    
+    # Add a horizontal line
+    hr(),
+    
+    # Add the first row of control widgets
+    fixedRow(
+      # Add radio buttons for user to specify the type of VCF files
+      column(6, add_vcf_type()),
+      
+      # Add a conditional panel for user to specify the column names in 
+      # Mutect VCFs which contain the tumor sample information (if needed)
+      column(6, conditionalPanel(
+        condition = "input.vcftype == 'mutect'",
+        add_tumor_col_names()))
+    ),
+    
+    # Add the next row of control widgets
+    fixedRow(
+      # Add radio buttons for user to specify the reference genome
+      column(6, add_reference_genome()),
+      
+      # Add radio buttons for user to specify the genomic region
+      # from where the VCFs were generated
+      column(6, add_region())
+    ),
+    
+    # Add the next row of control widgets
+    fixedRow(
+      # Add text input for user to specify the sample names
+      # representing different VCF files
+      column(6, add_sample_names()),
+      
+      # Add text input for user to specify the base filename
+      # of the CSV and PDF files generated
+      column(6, add_base_filename())
+    ),
+    
+    # Add the next row of control widgets
+    fixedRow(
+      # Add text input for user to specify the zip file name
+      column(6, add_zipfile_name()),
+      
+      # Add a file upload control for user to upload multiple VCF files
+      column(6, upload_vcf_files())),
+    
+    
+    # Add the next row of control widgets
+    fixedRow(column(6,
+                    MyDownloadButton(outputId = "download",
+                                     label = "Submit",
+                                     style="color: #fff; 
+                                       background-color: #337ab7; 
+                                       border-color: #2e6da4"),
+                    offset = 6)),
+    
+    # Add one line break
+    br(),
+    
+    fixedRow(column(6,
+                    actionButton(inputId = "remove", 
+                                 label = "Remove notifications"),
+                    offset = 6)),
+    
+    # Add one line break
+    br(),
+    
+    # Add a download button for user to download VCF files to test
+    fixedRow(column(6,
+                    downloadButton(outputId = "downloadsampleVCFs", 
+                                   label = "Download the sample VCFs"),
+                    offset = 6)),
+    
+    # Add one line break
+    br(),
+    
+    # Add a button for user to run ICAMS on sample Strelka SBS VCFs
+    fixedRow(column(6, 
+                    MyDownloadButton(outputId = "runstrelkasbsvcfs", 
+                                     label = 
+                                       paste0("Run ICAMS on two ", 
+                                              "1-sample Strelka SBS VCFs")),
+                    offset = 6)),
+    
+    # Add one line break
+    br(),
+    
+    # Add a button for user to run ICAMS on sample Mutect VCFs
+    fixedRow(column(6, 
+                    MyDownloadButton(outputId = "runmutectvcfs", 
+                                     label = 
+                                       paste0("Run ICAMS on two ", 
+                                              "1-sample Mutect VCFs")),
+                    offset = 6)),
+    
+    # Add a horizontal line
+    hr(),
+    
+    # Add a footer on the page showing the URL of ICAMS package
+    p("For complete documentation of ICAMS, please refer to ",
+      a(href = "https://cran.rstudio.com/web/packages/ICAMS/index.html",
+        "https://cran.rstudio.com/web/packages/ICAMS/index.html"))
+    
+  )
+}
 
 #' @import shiny
 golem_add_external_resources <- function(){
