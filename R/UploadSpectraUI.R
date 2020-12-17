040c145f74a92a59677b739573fe265f4461a6c5
Steven G. Rozen <steve@steverozen.net>
2020-12-17 18:29:30 +0800

    Changed layout of attr front page

--- R/UploadSpectraUI.R
+++ R/UploadSpectraUI.R
@@ -1,54 +1,50 @@
 #' @import shiny
 UploadSpectraUI <- function() {
-  # List the first level UI elements here
-  fixedPage(
-    # Add the first row of control widgets
-    fixedRow(
-      # Add radio buttons for user to specify the reference genome
-      column(6, AddReferenceGenome2()),
+  
+  sidebarLayout(
+    
+    sidebarPanel = sidebarPanel(
+      fluidRow(
+        # Add radio buttons for user to specify the reference genome
+        column(6, AddReferenceGenome2()),
+        
+        # Add radio buttons for user to specify the genomic region
+        # from where the catalogs were generated
+        column(6, AddRegion2())
+      ),
       
-      # Add radio buttons for user to specify the genomic region
-      # from where the catalogs were generated
-      column(6, AddRegion2())
-    ),
+      fluidRow(
+        column(6, UploadSpectra()),
+        
+        column(6, 
+               uiOutput(outputId = "showSpectraFromCatalog"), 
+               br(),
+               uiOutput(outputId = "sigAttributionFromCatalog"))
+      ), # end fluidRown
+      width = 4
+    ), # end sidebarPanel
     
-    # Add the next row of control widgets
-    fixedRow(
-      # Add a file upload control for user to upload spectra file
-      column(6,
-             div(tags$b("Load example spectra", style = "color: #337ab7;")),
-             br(),
-             splitLayout(cellWidths = c("20%", "20%", "20%", "20%"),
-                         actionButton(inputId = "preloadSBS96Spectra", 
-                                      label = "SBS96"),
-                         actionButton(inputId = "preloadSBS192Spectra", 
-                                      label = "SBS192"),
-                         actionButton(inputId = "preloadDBS78Spectra", 
-                                      label = "DBS78"),
-                         actionButton(inputId = "preloadIDSpectra", 
-                                      label = "ID"))
-      )
-    ),
-    
-    fixedRow(column(6, offset = 6,
-                    UploadSpectra(),
-                    downloadButton(outputId = "downloadSampleSpectra",
-                                   label = "Download example spectra"),
-    )),
-    
-    br(),
-    
-    # Add a download button for user to download sample spectra to test
-    fixedRow(column(6, offset = 6,
-                    splitLayout(cellWidths = c("30%", "70%"), 
-                                uiOutput(outputId = "showSpectraFromCatalog"), 
-                                uiOutput(outputId = "sigAttributionFromCatalog"))
-    )),
-    
-    br(),
-    
-    fixedRow(column(6, offset = 6, 
-                    uiOutput(outputId = "removeButton2")))
-    
-  )
-}
+    mainPanel = mainPanel(
+      fluidRow(
+        column(6,
+               wellPanel(
+                 div(tags$b("Load example spectra", style = "color: #337ab7;")),
+                 br(),
+                 splitLayout(cellWidths = c("20%", "20%", "20%", "20%"),
+                             actionButton(inputId = "preloadSBS96Spectra", 
+                                          label = "SBS96"),
+                             actionButton(inputId = "preloadSBS192Spectra", 
+                                          label = "SBS192"),
+                             actionButton(inputId = "preloadDBS78Spectra", 
+                                          label = "DBS78"),
+                             actionButton(inputId = "preloadIDSpectra", 
+                                          label = "ID")),
+                 br(),
+                 downloadButton(outputId = "downloadSampleSpectra",
+                                label = "Download example spectra"),
+               ) # End wellPanel
+        ) # end column
+      )# end fluidRow
+    ) # end mainPanel
+  ) # End sidebarLayout
+} # End function
